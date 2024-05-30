a5270d563fe747ad0c1a2f047c5dd619a2750f10
Amr Kahla <amrkahla6@gmail.com>
2024-05-30 20:51:47 +0300

    Start new game button

--- app.js
+++ app.js
@@ -15,10 +15,16 @@ const app = Vue.createApp({
     },computed:{
         monsterBarStyles()
         {
+            if(this.monsterHealth < 0){
+                return {width: '0%'} 
+            }
             return {width: this.monsterHealth + '%'};
         },
         playerBarStyles()
         {
+            if(this.playerHealth < 0){
+                return {width: '0%'};
+            }
             return {width: this.playerHealth + '%'};
         },
         myUseSpiaclAttach()
@@ -84,6 +90,16 @@ const app = Vue.createApp({
             }
 
             this.attachPlayer();
+        },
+        startNewGame()
+        {
+            this.monsterHealth = 100;
+            this.playerHealth  = 100;
+            this.currentRound  = 0;
+            this.winner        = null;
+        },
+        surrender(){
+            this.winner = 'monster'
         }
     },
 });
