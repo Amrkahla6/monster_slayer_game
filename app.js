cec2b3a968e15ed68aa678c615b260c148539de9
Amr Kahla <amrkahla6@gmail.com>
2024-05-30 19:47:41 +0300

    Adding a spiacl attach

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = Vue.createApp({
         return {
             playerHealth  : 100,
             monsterHealth : 100,
+            currentRound  : 0,
         }
     },computed:{
         monsterBarStyles()
@@ -18,10 +19,16 @@ const app = Vue.createApp({
         playerBarStyles()
         {
             return {width: this.playerHealth + '%'};
+        },
+        myUseSpiaclAttach()
+        {
+            return this.currentRound % 3 !== 0
         }
     },methods: {
         attachMonster()
         {
+            this.currentRound ++;
+
             //Random value between 5 and 12
             myAttachVlue = getRandomValue(5,12);
             this.monsterHealth  -= myAttachVlue;
@@ -32,6 +39,15 @@ const app = Vue.createApp({
             //Random value between 15 and 8
             myAttachVlue = getRandomValue(8,15);
             this.playerHealth  -= myAttachVlue;
+        },
+        speacialAttachMonster()
+        {
+            this.currentRound ++;
+
+            myAttachVlue = getRandomValue(10,25);
+            this.monsterHealth  -= myAttachVlue;
+            this.spicalAttch -- ;
+            this.attachPlayer();
         }
     },
 });
